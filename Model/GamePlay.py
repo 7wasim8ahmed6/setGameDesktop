81e7dc9f4877c8d694ee231751c740261b6f7407
raufwasimiqbalahmed <andrew.sim84@gmail.com>
2024-07-26 22:47:05 +0200

    implemented make a match

--- Model/GamePlay.py
+++ Model/GamePlay.py
@@ -6,9 +6,9 @@ from Model.Card import *
 
 class GamePlay:
     def __init__(self):
-        self.__cards: list[Card] = GamePlay.__createAllCards()
+        self.__cards = GamePlay.__createAllCards()
         shuffle(self.__cards)
-        self.__draw_cards: list[Card] = self.__cards[-12:]  # Take the last 12 cards
+        self.__draw_cards = self.__cards[-12:]  # Take the last 12 cards
         self.__cards = self.__cards[:-12]  # Remove the last 12 cards from __cards
         self.__matched: list[Card] = []
         self.__selected: list[Card] = []
@@ -33,3 +33,25 @@ class GamePlay:
 
     def get_matched_cards(self) -> [Card]:
         return self.__matched
+
+    def draw_cards(self):
+        if self.__cards:
+            self.__draw_cards = self.__cards[-3:]  # Take the last 3 cards
+            self.__cards = self.__cards[:-3]  # Remove the last 3 cards from __cards
+
+    def choose(self, card: Card):
+        pass
+
+    def makeMatch(self, card_a: Card, card_b: Card, card_c: Card) -> bool:
+        # Check if all three cards are different
+        if card_a.id == card_b.id or card_b.id == card_c.id or card_c.id == card_a.id:
+            return False
+
+        # Check if all attributes are either all the same or all different
+        attributes = ['shape', 'color', 'filling', 'number']
+        for attr in attributes:
+            values = {getattr(card_a, attr), getattr(card_b, attr), getattr(card_c, attr)}
+            if len(values) == 2:  # If there are exactly two different values, it's not a match
+                return False
+
+        return True
